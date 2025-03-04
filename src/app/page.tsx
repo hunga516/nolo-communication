202bcbae3d6568057b6a9b62d95c838f5b11bbb1
Ngoc Loc <hunga5160@gmail.com>
2025-03-04 19:23:07 +0700

    add shadcn ui and test tailwind css

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { nunito } from "./layout";
+
+export default function Home() {
+  return (
+   <div>
+    <span className={`${nunito.className} font-bold text-2xl bg-gradient-to-tr from-25% from-blue-500 to-80% to-blue-400 text-ellipsis text-transparent bg-clip-text`}>NOLO</span>
+    <div>hihi</div>
+   </div>
+  );
+}
