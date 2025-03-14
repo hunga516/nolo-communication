c0a46682e6e3a02a8d25fdce0de445ecc8215cef
Ngoc Loc <hunga5160@gmail.com>
2025-03-14 12:45:16 +0700

    delete import signout button

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,4 +1,4 @@
-import {SignedIn, SignedOut, SignInButton, SignOutButton, UserButton} from "@clerk/nextjs";
+import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 const AuthButton = () => {
     return (
