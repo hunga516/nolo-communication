3233ac4c9057d09bddea43e96fb028cbcc3d9825
Ngoc Loc <hunga5160@gmail.com>
2025-03-16 01:13:57 +0700

    finish logo transition

--- src/modules/home/ui/components/home-sidebar/app-sidebar.tsx
+++ src/modules/home/ui/components/home-sidebar/app-sidebar.tsx
@@ -4,19 +4,21 @@ import * as React from "react"
 import {
   AudioWaveform,
   Command,
-  GalleryVerticalEnd, Images,
+  GalleryVerticalEnd,
 } from "lucide-react"
 
 import {
   Sidebar,
-  SidebarContent, SidebarGroup,
-  SidebarHeader, SidebarMenu,
-  SidebarRail, SidebarTrigger,
+  SidebarContent,
+  SidebarHeader, SidebarMenu, SidebarMenuItem,
+  SidebarRail, useSidebar,
 } from "@/components/ui/sidebar"
 
 import {NavMain} from "@/modules/home/ui/components/home-sidebar/nav-main";
 import {NavProjects} from "@/modules/home/ui/components/home-sidebar/nav-projects";
 import Image from "next/image";
+import Link from "next/link";
+import {log} from "node:util";
 
 export const data = {
   teams: [
@@ -39,15 +41,22 @@ export const data = {
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+  const { open } = useSidebar()
+
+  console.log(open)
+
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <SidebarGroup>
-          <SidebarTrigger className="p-4"></SidebarTrigger>
-         <SidebarMenu>
-           <Image src="./logo.svg" alt="logo nolo" width={25} height={25}></Image>
-         </SidebarMenu>
-        </SidebarGroup>
+       <SidebarMenu>
+         <SidebarMenuItem>
+          <Link href="/" className={`flex items-center gap-2`}>
+            <Image src="./logo.svg" alt="logo nolo" width={30} height={30}></Image>
+            <span className={`font-semibold ${open ? "duration-1000 opacity-100" : "duration-100 opacity-0 pointer-events-none"}`} >NOLO Community</span>
+          </Link>
+         </SidebarMenuItem>
+       </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
         <NavMain/>
