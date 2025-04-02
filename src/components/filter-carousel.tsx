c49b2ee6a6bc1b85632e50740b556e21d03e12aa
hunga516 <hunga5160@gmail.com>
2025-04-03 00:18:35 +0700

    add login routing for category and skeleton when loading

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -10,7 +10,8 @@ import {
 } from "@/components/ui/carousel"
 import {Badge} from "@/components/ui/badge";
 import {cn} from "@/lib/utils";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Skeleton} from "@/components/ui/skeleton";
 
 interface FilterCarouselProps {
     value?: string | null
@@ -29,13 +30,28 @@ export const FilterCarousel = ({
     isLoading,
 }: FilterCarouselProps) => {
     const [api, setApi] = useState<CarouselApi>()
+    const [current, setCurrent] = useState(0)
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        if(!api) return;
+
+        setCount(api.scrollSnapList().length)
+        setCurrent(api.selectedScrollSnap() + 1)
+
+        api.on("select", () => {
+            setCurrent(api.selectedScrollSnap() + 1)
+        })
+    }, [api])
 
     return (
         <div className="relative -mx-4 px-4 md:max-w-[calc(100vw-var(--sidebar-width))] ">
+
             {/*Left fade*/}
             <div className={cn(
-                "absolute left-12 inset-y-0 w-12 z-10 bg-gradient-to-r from-white to-transparent",
-                false && "hidden"
+                "left-12 inset-y-0 w-12 z-10 bg-gradient-to-r from-white to-transparent",
+                current === 1 && "hidden",
+                current === 0 ? "hidden" : "absolute",
             )}>
 
             </div>
@@ -45,22 +61,37 @@ export const FilterCarousel = ({
                     align: "start",
                     dragFree: true,
                 }}
+                setApi={setApi}
                 className="px-10"
             >
                 <CarouselContent className="-ml-2 -mx-4">
-                <CarouselItem
-                        className="pl-6 basis-auto"
-                    >
-                        <Badge
-                            variant={value === null ? "default" : "secondary"}
-                            className="px-3 py-1 text-sm rounded-sm  select-none">
-                             Tất cả
-                        </Badge>
-                    </CarouselItem>
-                    {data.map(item => (
+                    {!isLoading && (
+                        <CarouselItem
+                            onClick={() => onSelect?.(null)}
+                            className="pl-6 basis-auto"
+                        >
+                            <Badge
+                                variant={!value ? "default" : "secondary"}
+                                className="px-3 py-1 text-sm rounded-sm  select-none">
+                                Tất cả
+                            </Badge>
+                        </CarouselItem>
+                    )}
+                    {isLoading &&
+                        Array.from({length: 12}).map((item, i) => (
+                            <CarouselItem
+                                className={`${i === 0 && "ml-4"} pl-2 basis-auto`}
+                                key={i}
+                            >
+                                <Skeleton className="rounded-lg px-3 py-4 h-full w-[100px]"  />
+                            </CarouselItem>
+                        ))
+                    }
+                    {!isLoading && data.map(item => (
                         <CarouselItem
                             className="pl-2 basis-auto"
                             key={item.value}
+                            onClick={() => onSelect?.(item.value)}
                         >
                             <Badge
                                 variant={value === item.value ? "default" : "secondary"}
@@ -71,14 +102,14 @@ export const FilterCarousel = ({
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className="left-0 z-20" />
-                <CarouselNext className="right-0 z-20" />
+                <CarouselPrevious className="left-0 z-20"/>
+                <CarouselNext className="right-0 z-20"/>
             </Carousel>
 
             {/*Right fade*/}
             <div className={cn(
                 "absolute right-12 inset-y-0 w-16 z-10 bg-gradient-to-l from-white to-transparent",
-                false && "hidden"
+                current === count && "hidden"
             )}>
             </div>
         </div>
--- src/modules/home/ui/sections/category-section.tsx
+++ src/modules/home/ui/sections/category-section.tsx
@@ -4,6 +4,7 @@ import {trpc} from "@/trpc/client";
 import {Suspense} from "react";
 import {ErrorBoundary} from "react-error-boundary";
 import {FilterCarousel} from "@/components/filter-carousel";
+import {useRouter} from "next/navigation";
 
 interface CategorySectionProps {
     categoryId: string;
@@ -11,7 +12,7 @@ interface CategorySectionProps {
 
 const CategoriesSection = ({categoryId} : CategorySectionProps) => {
     return (
-        <Suspense  fallback={<p>loading ...</p>}>
+        <Suspense  fallback={<FilterCarousel isLoading data={[]} value={null} />}>
             <ErrorBoundary fallback={<p>loi roi ban oi ...</p>}>
                 <CategoriesSectionSuspense categoryId={categoryId} />
             </ErrorBoundary>
@@ -20,14 +21,28 @@ const CategoriesSection = ({categoryId} : CategorySectionProps) => {
 }
 
 const CategoriesSectionSuspense = ({categoryId}: CategorySectionProps) => {
-    const [categories] = trpc.categories.getMany.useSuspenseQuery()
+    const router = useRouter()
+    const [categories] = trpc.categories.getMany.useSuspenseQuery() //useSuspenseQuery phai duoc boc o trong Suspen tag
 
     const newCategories = categories.map(category => ({
         label: category.name,
         value: JSON.stringify(category.id),
     }))
 
-    return <FilterCarousel value={categoryId} data={newCategories} />
+    const onSelect = (value?: string | null) => {
+        const url = new URL(window.location.href)
+
+        if (value) {
+            url.searchParams.set("categoryId", value)
+        }
+        else {
+            url.searchParams.delete("categoryId")
+        }
+
+        router.push(url.href)
+    }
+
+    return <FilterCarousel onSelect={onSelect} value={categoryId} data={newCategories} />
 }
 
 export default CategoriesSection;
