aa128b0b1f233c1e8d305ef18d8dd09545bf6aed
hunga516 <hunga5160@gmail.com>
2025-04-08 01:16:20 +0700

    add table and infinity loading scroll fetch

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -5,6 +5,9 @@ import { DEFAULT_LIMIT } from "@/constans";
 import {ErrorBoundary} from "react-error-boundary";
 import {Suspense} from "react";
 import {InfiniteScroll} from "@/components/infinite-scroll";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {useRouter} from "next/navigation";
+import Link from "next/link";
 
 
 const VideosSection = () => {
@@ -18,7 +21,8 @@ const VideosSection = () => {
 }
 
 const VideosSectionSuspense = () => {
-  const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    const router = useRouter();
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -29,7 +33,50 @@ const VideosSectionSuspense = () => {
 
   return (
       <div>
-          {JSON.stringify(data)}
+          <div className="border-y">
+              <Table>
+                  <TableHeader>
+                      <TableRow>
+                          <TableHead className="pl-6">Video</TableHead>
+                          <TableHead>Visibility</TableHead>
+                          <TableHead>Status</TableHead>
+                          <TableHead>Date</TableHead>
+                          <TableHead className="text-right">Views</TableHead>
+                          <TableHead className="text-right">Comments</TableHead>
+                          <TableHead className="text-right pr-6">Likes</TableHead>
+                      </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                      {videos.pages.flatMap((page) => page.items).map((video) => (
+                         <Link key={video.id} href={`/videos/${video.id}`} legacyBehavior>
+                             <TableRow>
+                                 <TableCell>
+                                     {video.title}
+                                 </TableCell>
+                                 <TableCell>
+                                     visibility
+                                 </TableCell>
+                                 <TableCell>
+                                     Status
+                                 </TableCell>
+                                 <TableCell>
+                                     Date
+                                 </TableCell>
+                                 <TableCell>
+                                     Views
+                                 </TableCell>
+                                 <TableCell>
+                                     Comments
+                                 </TableCell>
+                                 <TableCell>
+                                     Likes
+                                 </TableCell>
+                             </TableRow>
+                         </Link>
+                      ))}
+                  </TableBody>
+              </Table>
+          </div>
           <InfiniteScroll isManual hasNextPage={query.hasNextPage} isFetchingNextPage={query.isFetchingNextPage} fetchNextPage={query.fetchNextPage} />
       </div>
   );
--- src/modules/studio/view/studio-view.tsx
+++ src/modules/studio/view/studio-view.tsx
@@ -2,7 +2,13 @@ import VideosSection from "@/modules/studio/ui/sections/videos-section";
 
 const StudioView = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-y-6 pt-2.5">
+        <div className="px-4">
+            <h1 className="text-2xl font-bold">Nội dung</h1>
+            <p className="text-sm text-muted-foreground">
+                Quản lý các nội dung và video của bạn
+            </p>
+        </div>
       <VideosSection />
     </div>
   );
