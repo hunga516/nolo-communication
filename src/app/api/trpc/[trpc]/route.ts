6439e484fc23a4c1f4c3385578dc2178621cf4b1
hunga516 <hunga5160@gmail.com>
2025-04-13 12:48:08 +0700

    update

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,33 +1,11 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { createTRPCContext } from "@/trpc/init";
 import { appRouter } from "@/trpc/routers/_app";
-
-// handler cho GET và POST requests
-const handler = async (req: Request) => {
-  try {
-    // Đọc query từ URL trong request
-    const url = new URL(req.url);
-    const query = url.searchParams;
-
-    // Kiểm tra nếu query không có
-    if (!query || query.toString() === "") {
-      return new Response(JSON.stringify({ error: "Query is undefined" }), {
-        status: 400,
-      });
-    }
-
-    // Gọi fetchRequestHandler để xử lý request TRPC
-    return await fetchRequestHandler({
-      endpoint: "/api/trpc",
-      req,
-      router: appRouter,
-      createContext: createTRPCContext,
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error }), {
-      status: 500,
-    });
-  }
-};
-
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: createTRPCContext,
+  });
 export { handler as GET, handler as POST };
