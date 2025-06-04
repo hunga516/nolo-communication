698d08dde266efa0c76502210a17fa4e0687fbf0
Le Loc <hunga5160@gmail.com>
2025-06-04 07:19:06 +0700

    change path quest

--- src/app/(home)/nhiem-vu/page.tsx
+++ src/app/(home)/nhiem-vu/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
     return (
         <div>
-            <iframe className="w-full h-screen" src="http://localhost:3001/" />
+            <iframe className="w-full h-screen" src="https://vi-education-git-for-nolo-community-hunga516s-projects.vercel.app/" />
         </div>
     );
 }
